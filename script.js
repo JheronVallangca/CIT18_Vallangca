e17bfb30fa1c6d5df1af1f28b71b7a28e8bf3bbb
Jheron <jdv1104@student.uc-bcf.edu.ph>
2024-05-20 18:23:03 +0800

    With Updated Js

---
+++ script.js
@@ -0,0 +1,29 @@
+const sr = ScrollReveal({
+    distance: '100px',
+    duration: 1500,
+    easing: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
+    interval: 100, 
+});
+
+sr.reveal('.PersonalDesign .Personalboxone', {
+    origin: 'top',
+    delay:150,  
+});
+
+sr.reveal('.PersonalDesign .Personalboxtwo', {
+    origin: 'bottom',
+});
+
+sr.reveal('.AboutDesign .Aboutboxone', { 
+    origin: 'right',
+});
+
+sr.reveal('.AboutDesign .Aboutboxtwo', { 
+    origin: 'right',
+    delay: 200,
+});
+
+sr.reveal('.AboutDesign .Aboutboxthree', { 
+    origin: 'right',
+    delay: 400,
+});
